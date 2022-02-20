8568889254475cd1ff703d869998261e82ab2587
zwb <1748244285@qq.com>
2022-02-20 13:19:10 +0800

    完成静态页面组件化

--- projectShoping/app2.0/src/main.js
+++ projectShoping/app2.0/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//注册三级联动为全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+//名称 及 组件
+Vue.component(TypeNav.name,TypeNav);
+
 Vue.config.productionTip = false
 
 new Vue({
